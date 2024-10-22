55b3186ef36920d850097100078c3529064bdbbb
leehengpan <leehengpan@gmail.com>
2024-10-22 16:56:23 -0400

    update function names in reforms.py

--- policyengine_us/reforms/reforms.py
+++ policyengine_us/reforms/reforms.py
@@ -5,50 +5,50 @@ from .dc_tax_threshold_joint_ratio import (
     create_dc_tax_threshold_joint_ratio_reform,
 )
 from .congress.romney.family_security_act import (
-    create_remove_head_of_household_reform,
+    create_remove_head_of_household,
 )
 from .cbo.payroll import (
     create_increase_taxable_earnings_for_social_security_reform,
 )
-from .congress.wyden_smith import create_ctc_expansion_reform
-from .federal import create_abolish_federal_income_tax_reform
-from .federal import create_abolish_payroll_tax_reform
-from .federal import create_reported_state_income_tax_reform
+from .congress.wyden_smith import create_ctc_expansion
+from .federal import create_abolish_federal_income_tax
+from .federal import create_abolish_payroll_tax
+from .federal import create_reported_state_income_tax
 from .biden.budget_2025 import (
     create_medicare_and_investment_tax_increase_reform,
 )
-from .biden.budget_2025 import create_capital_gains_tax_increase_reform
-from .eitc import create_halve_joint_eitc_phase_out_rate_reform
-from .states.ny.wftc import create_ny_working_families_tax_credit_reform
+from .biden.budget_2025 import create_capital_gains_tax_increase
+from .eitc import create_halve_joint_eitc_phase_out_rate
+from .states.ny.wftc import create_ny_working_families_tax_credit
 from .states.dc.dc_ctc import (
-    create_dc_ctc_reform,
+    create_dc_ctc,
 )
 from .harris.lift.middle_class_tax_credit import (
-    create_middle_class_tax_credit_reform,
+    create_middle_class_tax_credit,
 )
 from .harris.rent_relief_act.rent_relief_tax_credit import (
-    create_rent_relief_tax_credit_reform,
+    create_rent_relief_tax_credit,
 )
 from .congress.tlaib import (
-    create_end_child_poverty_act_reform,
+    create_end_child_poverty_act,
 )
 from .congress.tlaib.boost import (
-    create_boost_middle_class_tax_credit_reform,
+    create_boost_middle_class_tax_credit,
 )
 from .states.mn.walz import (
-    create_mn_walz_hf1938_repeal_reform,
+    create_mn_walz_hf1938_repeal,
 )
 from .states.oregon.rebate import (
-    create_or_rebate_state_tax_exempt_reform,
+    create_or_rebate_state_tax_exempt,
 )
 from .congress.romney.family_security_act_2024.ctc import (
-    create_family_security_act_2024_ctc_reform,
+    create_family_security_act_2024_ctc,
 )
 from .congress.romney.family_security_act_2024.eitc import (
-    create_family_security_act_2024_eitc_reform,
+    create_family_security_act_2024_eitc,
 )
 from .treasury.repeal_dependent_exemptions import (
-    create_repeal_dependent_exemptions_reform,
+    create_repeal_dependent_exemptions,
 )
 
 from policyengine_core.reforms import Reform
@@ -64,7 +64,7 @@ def create_structural_reforms_from_parameters(parameters, period):
     dc_tax_threshold_joint_ratio_reform = (
         create_dc_tax_threshold_joint_ratio_reform(parameters, period)
     )
-    remove_head_of_household = create_remove_head_of_household_reform(
+    remove_head_of_household = create_remove_head_of_household(
         parameters, period
     )
     increase_taxable_earnings_for_social_security_reform = (
@@ -75,49 +75,45 @@ def create_structural_reforms_from_parameters(parameters, period):
     medicare_and_investment_tax_increase = (
         create_medicare_and_investment_tax_increase_reform(parameters, period)
     )
-    ctc_expansion = create_ctc_expansion_reform(parameters, period)
+    ctc_expansion = create_ctc_expansion(parameters, period)
 
-    abolish_federal_income_tax = create_abolish_federal_income_tax_reform(
+    abolish_federal_income_tax = create_abolish_federal_income_tax(
         parameters, period
     )
-    abolish_payroll_tax = create_abolish_payroll_tax_reform(parameters, period)
-    reported_state_income_tax = create_reported_state_income_tax_reform(
+    abolish_payroll_tax = create_abolish_payroll_tax(parameters, period)
+    reported_state_income_tax = create_reported_state_income_tax(
         parameters, period
     )
-    capital_gains_tax_increase = create_capital_gains_tax_increase_reform(
+    capital_gains_tax_increase = create_capital_gains_tax_increase(
         parameters, period
     )
-    halve_joint_eitc_phase_out_rate = (
-        create_halve_joint_eitc_phase_out_rate_reform(parameters, period)
+    halve_joint_eitc_phase_out_rate = create_halve_joint_eitc_phase_out_rate(
+        parameters, period
     )
-    ny_wftc = create_ny_working_families_tax_credit_reform(parameters, period)
+    ny_wftc = create_ny_working_families_tax_credit(parameters, period)
 
     dc_ctc = create_dc_ctc_reform(parameters, period)
 
-    middle_class_tax_credit = create_middle_class_tax_credit_reform(
+    middle_class_tax_credit = create_middle_class_tax_credit(
         parameters, period
     )
-    rent_relief_tax_credit = create_rent_relief_tax_credit_reform(
+    rent_relief_tax_credit = create_rent_relief_tax_credit(parameters, period)
+    end_child_poverty_act = create_end_child_poverty_act(parameters, period)
+    boost_middle_class_tax_credit = create_boost_middle_class_tax_credit(
         parameters, period
     )
-    end_child_poverty_act = create_end_child_poverty_act_reform(
-        parameters, period
-    )
-    boost_middle_class_tax_credit = (
-        create_boost_middle_class_tax_credit_reform(parameters, period)
-    )
-    mn_walz_hf1938 = create_mn_walz_hf1938_repeal_reform(parameters, period)
+    mn_walz_hf1938 = create_mn_walz_hf1938_repeal(parameters, period)
 
-    or_rebate_state_tax_exempt = create_or_rebate_state_tax_exempt_reform(
+    or_rebate_state_tax_exempt = create_or_rebate_state_tax_exempt(
         parameters, period
     )
-    family_security_act_2024_ctc = create_family_security_act_2024_ctc_reform(
+    family_security_act_2024_ctc = create_family_security_act_2024_ctc(
         parameters, period
     )
-    family_security_act_2024_eitc = (
-        create_family_security_act_2024_eitc_reform(parameters, period)
+    family_security_act_2024_eitc = create_family_security_act_2024_eitc(
+        parameters, period
     )
-    repeal_dependent_exemptions = create_repeal_dependent_exemptions_reform(
+    repeal_dependent_exemptions = create_repeal_dependent_exemptions(
         parameters, period
     )
 
