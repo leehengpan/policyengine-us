c0589f852532c4ea3ab2b88759f4d47889629626
leehengpan <leehengpan@gmail.com>
2024-10-22 16:10:25 -0400

    Handle reform functions with and without arguments

--- policyengine_us/reforms/congress/romney/family_security_act/remove_head_of_household.py
+++ policyengine_us/reforms/congress/romney/family_security_act/remove_head_of_household.py
@@ -10,22 +10,6 @@ def create_remove_head_of_household() -> Reform:
     return reform
 
 
-def create_remove_head_of_household_reform(
-    parameters, period, bypass: bool = False
-):
-    if bypass or parameters is None:
-        return create_remove_head_of_household()
-
-    # Look ahead for the next five years
-    p = parameters.gov.contrib.congress.romney.family_security_act
-    reform_active = reform_is_active(p, period, 5, "remove_head_of_household")
-
-    if reform_active:
-        return create_remove_head_of_household()
-    else:
-        return None
-
-
 remove_head_of_household = create_reform_if_active(
     None,
     None,
